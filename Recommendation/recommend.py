44c65ae4899a2933d2f03bfcdebdd9a528c058f2
Cookie890416 <68961770+Cookie890416@users.noreply.github.com>
2021-06-03 17:05:11 +0800

    RecommendSight

---
+++ Recommendation/recommend.py
@@ -0,0 +1,10 @@
+import json
+from flask_pymongo import pymongo
+from flask import jsonify,request,Blueprint
+from Recommendation.RecommendPoint import nearest_ViewPoint
+recommend = Blueprint('recommend',__name__)
+@recommend.route('/recommendScene')
+def recommandScene():
+    y=request.args["longitude"]
+    x=request.args["latitude"]
+    return jsonify(nearest_ViewPoint(x,y))
